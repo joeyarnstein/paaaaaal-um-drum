7b374175bbc860f84acaabc1d93d3dde7d1cad50
Joey Arnstein and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-20 15:50:59 -0800

    add functionality for values that are not palindromes

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Behavior                           Input                     Output
+// return false for not palindromes      dog                   false
+// retrun true for palindromes           tacocat               true
+
+
+
+//grab input from user
+//turn it into an array of letters
+//clone the array and reverse it
+//see if the original array equals the reversed array
+//tell user it is palindrome or not
+
+
+var palindromeChecker = function(userInput) {
+  return false;
+
+}
---
+++ specs/specs.js
@@ -0,0 +1,6 @@
+describe('palindromeChecker', function () {
+  it('returns false for input that is not a palindrome', function() {
+    expect(palindromeChecker('dog')).to.equal(false);
+  });
+
+});
